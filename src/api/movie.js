88a2f134bb84a8de0baa2c6fd2dd887f79189fca
liuliu <awoo.chiaminliu@gmail.com>
2019-06-04 20:45:27 +0800

    [add] search bar on the page.
    Use for movie search.

--- src/api/movie.js
+++ src/api/movie.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 module.exports = {
-    async requestMovieInfo () {
-        let {data : info} = await axios.get("http://www.omdbapi.com/?s=Batman&apikey=34472924")
+    async requestMovieInfo (search = "Batman") {
+        let {data : info} = await axios.get(`http://www.omdbapi.com/?s=${search}&apikey=34472924`)
         return info
     }
 }
--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom";
 import Header from './components/Header';
 import Todo from './components/Todo';
 import api from './api/base';
+import InputSearch from "./components/InputSearch"
 import Movie from "./api/movie";
 import Product from "./components/Product";
+import Product_2 from "./components/Product_2";
 
 
 
@@ -14,8 +16,17 @@ class HelloMessage extends React.Component {
     super(props);
     this.state = {
       todos: [],
-      movies: []
+      movies: [],
     };
+
+  }
+
+  async searchKwd (kwd) {
+    if (!kwd) kwd = "banana";
+
+    this.setState({
+      movies: await Movie.requestMovieInfo(kwd)
+    })
   }
 
   async componentDidMount() {
@@ -35,8 +46,10 @@ class HelloMessage extends React.Component {
     return (
       <div>
         <Header/>
+        <InputSearch inputKwd = { (kwd) => this.searchKwd(kwd)} />
         {/* <Todo todos = {todos} /> */}
         <Product movies = {movies} />
+        {/* <Product_2 movies = {movies} /> */}
         <div className="container">
             <h1>Hi {this.props.name}</h1>
         </div>
