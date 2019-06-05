61d3ec7be292682220cca5a6d96b2661328e8913
youli <soso830808@gmail.com>
2019-06-05 16:32:31 +0800

    fix TabsControl

--- src/components/TabsControl.js
+++ src/components/TabsControl.js
@@ -1,45 +1,61 @@
 import React from "react";
-var createReactClass = require('create-react-class');
-let TabsControl = createReactClass({
-    getInitialState: function () {
-        return { currentIndex: 0 }
-    },
-    getTitleItemCssClasses: function (index) {
-        return index === this.state.currentIndex ? "tab-title-item active" : "tab-title-item";
-    },
 
-    getContentItemCssClasses: function (index) {
-        return index === this.state.currentIndex ? "tab-content-item active" : "tab-content-item";
-    },
 
-    render: function () {
-        let that = this;
-        return (
-            <div>
-                <nav className="tab-title-items">
 
-                    {React.Children.map(this.props.children, (element, index) => {
-                        return (<div onClick={() => { this.setState({ currentIndex: index }) }} className={that.getTitleItemCssClasses(index)}>{element.props.name}</div>)
-                    })}
-                </nav>
-                <div className="tab-content-items">
-                    {React.Children.map(this.props.children, (element, index) => {
-                        return (<div className={that.getContentItemCssClasses(index)}>{element}</div>)
-                    })}
-                </div>
+const TabsControl = (props) => {
+    var currentIndex = 0;
+    const setClass = (index, name) => {
+
+        var elems = document.querySelectorAll(".tab-title-item");
+        elems.forEach(function (e) {
+            e.classList.remove("active");
+            if (e.id === name) {
+                e.classList.add("active");
+            }
+        })
+        var elems = document.querySelectorAll(".tab-content-item");
+        elems.forEach(function (e) {
+            e.classList.remove("active");
+            if (e.id === name) {
+                e.classList.add("active");
+            }
+        })
+
+
+    }
+    const getTitleItemCssClasses = (index) => {
+        return index === currentIndex ? "tab-title-item active" : "tab-title-item";
+    }
+    const getContentItemCssClasses = (index) => {
+        return index === currentIndex ? "tab-content-item active" : "tab-content-item";
+    }
+    return (
+        <div>
+            <nav className="tab-title-items">
+
+                {React.Children.map(props.children, (element, index) => {
+                    return (
+                        <div onClick={() => setClass(index, element.props.name)} id={element.props.name} key={element.props.name} className={getTitleItemCssClasses(index)}>
+                            {element.props.name}
+                        </div>
+                    )
+                })}
+            </nav>
+            <div className="tab-content-items">
+                {React.Children.map(props.children, (element, index) => {
+                    return (<div data={element.props.name} id={element.props.name} className={getContentItemCssClasses(index)}>{element}</div>)
+                })}
             </div>
-        )
-    }
-});
+        </div>
+    )
+}
 
-let Tab = createReactClass({
-    render: function () {
-        return (
-            <div>{this.props.children}</div>
-        );
-    }
-});
 
+const Tab = (props) => {
+    return (
+        <div>{props.children}</div>
+    );
+};
 module.exports = {
     TabsControl,
     Tab,
