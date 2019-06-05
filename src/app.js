117d151a400a1bff8ecfe111352734cd0b7f6b82
kamili <awoo.kamili@gmail.com>
2019-06-05 17:21:32 +0800

    kami awoo react homework

--- src/app.js
+++ src/app.js
@@ -1,38 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Header from './components/Header';
+import Content from './components/Content';
 import api from './api/base';
-
-
 
 class HelloMessage extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: []
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: []
+        };
+    }
 
-  async componentDidMount() {
-    const todos = await api.reloadTodoDatas();
-    this.setState({
-      todos
-    });
-  }
+    async componentDidMount() {
+        const todos = await api.reloadTodoDatas();
+        this.setState({
+            todos
+        });
+    }
 
-  render() {
-    const { todos } = this.state;
-    return (
-      <div>
-        <Header/>
-        <div className="container">
-            <h1>Hi {this.props.name}</h1>
-        </div>
+    render() {
+        const { todos } = this.state;
+        return (
+            <div>
+                <Header />
+                <div className="container">
+                    <h1>God of {this.props.name}</h1>
+                </div>
+                <Content todos={todos}/>
+                <div>
       </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 const App = document.getElementById("app");
-ReactDOM.render(<HelloMessage name="Caesar" />, App);
+ReactDOM.render(
+<HelloMessage name="Kami" />, App
+);
